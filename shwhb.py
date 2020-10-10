4cd571afb83417ea23c822754b3dfc3765ca2cbd
aleksey-dvlp <54710399+aleksey-dvlp@users.noreply.github.com>
2020-10-10 15:30:33 +0300

    Create shwhb.py

---
+++ shwhb.py
@@ -0,0 +1,38 @@
+#CHOOSE ME HERE
+#any simbol you want
+#just print me inside ' '
+
+s = 'o'
+
+#------------------
+print(' ',s,s,s,' ',' ',s,s,'{:50}'.format(' '),s,s)
+print(s,s,' ',s,s,' ',' ',s,s,'{:44}'.format(' '),s,s,s,s,s)
+print(s,s,'{:9}'.format(' '),s,s,'{:44}'.format(' '),s,s,s,s,s)
+print(' ',s,s,'{:7}'.format(' '),s,s,' ',' ',s,s,s, '{:14}'.format(' '),s,s,s,' ',' ',' ',s,s,s,s,s,' ',s,s,' ',s,s,s,s,s,' ',s,s)
+print(' ',' ',s,s,' ',' ',' ',s,s,'{:7}'.format(' '),s,s,s,'{:12}'.format(' '),s,s,' ',' ',s,s,s,s,s,s,' ',s,s,' ',s,s,' ',' ',s,s,s,s)
+print(' ',' ',' ',s,s,' ',' ',s,s,s,s,s,' ',' ',s,s,'{:14}'.format(' '),s,s,' ',s,s,' ',' ',s,s,' ',s,s,' ',s,s,' ',' ',' ',s,s,s)
+print('{:5}'.format(' '),s,s,' ',' ',s,s,s,s,s,s,' ',s,s,'{:14}'.format(' '),s,s,' ',s,s,s,s,s,s,' ',s,s,' ',s,s,'{:7}'.format(' '),s,s)
+print('{:5}'.format(' '),s,s,' ',' ',s,s,' ',' ',s,s,' ',' ',s,s,' ',' ',s,s,' ',' ','',s,s,' ',s,s,'{:9}'.format(' '), s,s, ' ',s,s,' ',' ',s,s,s,s)
+print(s,s,s,s,s,' ',' ',s,s,' ',' ',s,s,' ',' ',s,s,s,s,s,s,s,s,s,s,' ','',s,s,s,s,s,s,' ',s,s,s,' ',s,s,s,s,s,s,s,s)
+print(' ',s,s,s,' ',' ',s,s,' ',' ',' ',s,s, ' ',' ',' ',s,s,s, ' ',' ',s,s,s,' ',' ',' ','',s,s,s,s,' ',' ',s,s,s,' ',' ',s,s,s,s,' ',s,s,s)
+print()
+
+print(s,s,' ',' ',s,s,' ',' ',s,s,s,s,' ',s,s,' ',s,s,s,s,s,' ',' ',s,s,s,s,s,' ',' ',s,s,' ',s,s)
+print(s,s,' ',' ',s,s,' ',s,s,s,s,s,s,s,s,' ',s,s,s,s,s,s,' ',s,s,s,s,s,s,' ',s,s,' ',s,s)
+print(s,s,s,s,s,s,' ',s,s,' ',' ',' ',s,s,s,' ',s,s,' ',' ',s,s,' ',s,s,' ',' ',s,s,' ',s,s,' ',s,s)
+print(s,s,s,s,s,s,' ',s,s,' ',' ',s,s,s,s,' ',s,s,s,s,s,' ',' ',s,s,s,s,s,' ',' ',' ',s,s,s,s)
+print(s,s,' ',' ',s,s,' ',s,s,s,s,s,s,s,s,' ',s,s,s,s,' ',' ',' ',s,s,s,s,' ',' ',s,s,s,s,s)
+print(s,s,' ',' ',s,s, ' ',' ',s,s,s,s,' ',s,s,' ',s,s,'{:9}'.format(' '),s,s,'{:7}'.format(' '),s,s,s,s)
+print()
+
+print(s,s,'{:9}'.format(' '),s,s,'{:15}'.format(' '),s,s,' ',' ',' ',s,s,'{:17}'.format(' '),s,s)
+print(s,s,'{:9}'.format(' '),s,s,'{:13}'.format(' '),s,s,s,s,' ',' ',s,s,'{:17}'.format(' '),s,s)
+print(s,s,'{:27}'.format(' '),s,s,s,s,' ',' ',s,s,'{:17}'.format(' '),s,s)
+
+print(s,s,s,s,s,' ',' ',s,s,' ',s,s,' ',s,s,' ',' ',s,s,' ',' ',' ',s,s,' ',s,s,' ',' ',' ',s,s,s,s,s,' ',' ',s,s,s,s,' ',s,s,' ',' ',s,s,' ',s,s)
+print(s,s,s,s,s,s,' ',s,s,' ',s,s,s,s,s,s,' ',s,s,' ',' ',' ',s,s,s,s,s,s,' ',s,s,s,s,s,s,' ',s,s,s,s,s,s,s,s,' ',' ',s,s,' ',s,s)
+print(s,s,' ',' ',s,s,' ',s,s,' ',s,s,s,' ',s,s,' ',s,s,' ',' ',' ',s,s,s,s,s,s,' ',s,s,' ',' ',s,s,' ',s,s,' ',' ',' ',s,s,s,' ',' ',s,s,' ',s,s)
+print(s,s,' ',' ',s,s,' ',s,s,' ',s,s,' ',' ',' ',' ',' ',s,s,' ',' ',' ',s,s,' ',' ',s,s,' ',s,s,' ',' ',s,s,' ',s,s,' ',' ',s,s,s,s,' ',' ',' ',s,s,s,s)
+print(s,s,s,s,s,s,' ',s,s,' ',s,s,'{:9}'.format(' '),s,s,s,s,' ',s,s,' ',' ',s,s,' ',s,s,s,s,s,s,' ',s,s,s,s,s,s,s,s,' ',s,s,s,s,s)
+print(s,s,s,s,s,' ',' ',s,s,' ',s,s,'{:11}'.format(' '),s,s,s,' ',s,s,' ',' ',s,s,' ',' ',s,s,s,s,s,' ',' ',s,s,s,s,' ',s,s,' ',s,s,s,s)
+
